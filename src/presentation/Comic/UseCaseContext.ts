3e35d3aed89c518e02b13bec183535e9218d2647
sergey_malykh <sergeymalykhdm@gmail.com>
2025-05-24 19:58:00 +0600

    feat: add GetComic to presentation

--- src/presentation/Comic/UseCaseContext.ts
+++ src/presentation/Comic/UseCaseContext.ts
@@ -1,14 +1,19 @@
 import { createContext, useContext } from "react";
+import type IGetComic from "../../domain/Comic/GetComic";
+import { GetComic } from "../../domain/Comic/GetComic";
+import type IGetComicsList from "../../domain/Comic/GetComicsList";
 import { GetComicsList } from "../../domain/Comic/GetComicsList";
 import MockComicsRepository from "../../infrastructure/Comic/MockComicsRepository";
 
 type UseCases = {
-    getComicsList: GetComicsList;
+    getComicsList: IGetComicsList;
+    getComic: IGetComic;
 };
 const comicsRepository = new MockComicsRepository();
 const getComicsList = new GetComicsList(comicsRepository);
+const getComic = new GetComic(comicsRepository);
 
-const UseCasesContext = createContext<UseCases>({ getComicsList: getComicsList });
+const UseCasesContext = createContext<UseCases>({ getComicsList, getComic });
 
 export function useCases(): UseCases {
     return useContext(UseCasesContext);
