2006f2abb80baf98c4769c6b62d95ee85011768d
sergey_malykh <sergeymalykhdm@gmail.com>
2025-05-26 19:33:21 +0600

    refactor: change routing config type to useRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, useRoutes, type RouteObject } from 'react-router-dom'
 import './App.css'
 import Layout from './Layout'
 import { AllComics } from './presentation/Comic/AllComics/AllComics'
 import { ComicPage } from './presentation/Comic/ComicPage/ComicPage'
 import { Favorite } from './presentation/Comic/Favorite/Favorite'
 
-function App() {
+const AppRoutes = () => {
+  const routes: RouteObject[] = [
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        { index: true, element: <AllComics /> },
+        { path: '/comic/:id', element: <ComicPage /> },
+        { path: '/favorite', element: <Favorite /> },
+      ],
+    },
+  ];
 
+  return useRoutes(routes);
+};
+
+function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Layout />}>
-            <Route path="/" element={<AllComics />} />
-            <Route path="/comic/:id" element={<ComicPage />} />
-            <Route path="/favorite" element={<Favorite />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
